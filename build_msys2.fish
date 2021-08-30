d95e36ce10135045f454c0dbd59fa27a1219a383
nyfair <nyfair2012@gmail.com>
2021-08-30 17:41:00 +0800

    Create msys2-runtime.yml

---
+++ build_msys2.fish
@@ -0,0 +1,8 @@
+git config --global user.email "test@test.com"
+git config --global user.name "test"
+curl -OL https://github.com/msys2/MSYS2-packages/archive/refs/heads/master.zip
+bsdtar xvf master.zip
+cd MSYS2-packages-master/msys2-runtime
+sed -i '/apply_git_am_with_msg /ised -i \'s/NAME_MAX 255/NAME_MAX 1024/\' winsup/cygwin/include/cygwin/limits.h' PKGBUILD
+cat PKGBUILD
+MSYSTEM=msys2 makepkg -s --noconfirm
