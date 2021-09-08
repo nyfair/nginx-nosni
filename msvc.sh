1958e3cec2495c3e5352faaa925dabe6bf26b02b
nyfair <nyfair2012@gmail.com>
2021-09-08 15:28:54 +0800

    migrate to openssl3

--- msvc.sh
+++ msvc.sh
@@ -1,8 +1,8 @@
 curl -OL https://github.com/nginx/nginx/archive/release-$_nginxver.tar.gz
-curl -OL https://github.com/openssl/openssl/archive/OpenSSL_$_opensslver.tar.gz
+curl -OL https://github.com/openssl/openssl/archive/openssl-$_opensslver.tar.gz
 curl -OL https://zlib.net/zlib-$_zlibver.tar.xz
 for i in *tar*; do bsdtar xf $i; done
-sed -i 's/MT/MD/g' openssl-OpenSSL_$_opensslver/Configurations/10-main.conf
+sed -i 's/MT/MD/g' openssl-openssl-$_opensslver/Configurations/10-main.conf
 cd nginx-release-$_nginxver
 sed -i 's/MT/MD/g' auto/cc/msvc
 auto/configure \
@@ -16,5 +16,5 @@ auto/configure \
   --http-client-body-temp-path=client_body_temp \
   --http-proxy-temp-path=proxy_temp \
   --with-zlib=../zlib-$_zlibver \
-  --with-openssl=../openssl-OpenSSL_$_opensslver \
+  --with-openssl=../openssl-openssl-$_opensslver \
   --with-cc-opt=-DFD_SETSIZE=65536 ${_module}
