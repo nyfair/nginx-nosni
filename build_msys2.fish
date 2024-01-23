af933b4195751722d887e2bd7065b9bf120baa69
nyfair <nyfair2012@gmail.com>
2024-01-23 23:49:47 +0800

    repatch runtime

--- build_msys2.fish
+++ build_msys2.fish
@@ -3,7 +3,5 @@ git config --global user.name "test"
 curl -OL https://github.com/msys2/MSYS2-packages/archive/refs/heads/master.zip
 bsdtar xvf master.zip
 cd MSYS2-packages-master/msys2-runtime
-sed -i '/apply_git_am_with_msg /ised -i \'s/NAME_MAX 255/NAME_MAX 1024/\' winsup/cygwin/include/cygwin/limits.h' PKGBUILD
-sed -i '/fixup-Add-functionality-for-converting-UNIX/d' PKGBUILD
-cat PKGBUILD
+mv ../../PKGBUILD-msys2 ./PKGBUILD
 MSYSTEM=msys2 makepkg --skipchecksums -s --noconfirm
