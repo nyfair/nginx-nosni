39fb7961128ccadf3a06260a2d05c196d3c1059b
nyfair <nyfair2012@gmail.com>
2024-01-23 22:33:20 +0800

    Update build_msys2.fish

--- build_msys2.fish
+++ build_msys2.fish
@@ -4,5 +4,6 @@ curl -OL https://github.com/msys2/MSYS2-packages/archive/refs/heads/master.zip
 bsdtar xvf master.zip
 cd MSYS2-packages-master/msys2-runtime
 sed -i '/apply_git_am_with_msg /ised -i \'s/NAME_MAX 255/NAME_MAX 1024/\' winsup/cygwin/include/cygwin/limits.h' PKGBUILD
+sed -i '/fixup-Add-functionality-for-converting-UNIX/d' PKGBUILD
 cat PKGBUILD
 MSYSTEM=msys2 makepkg -s --noconfirm
