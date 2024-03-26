fbe00f33d05fb66bd146f4efca2d1a1b66550b7e
nyfair <nyfair2012@gmail.com>
2024-03-26 21:16:02 +0800

    update runtime patchset

--- build_msys2.fish
+++ build_msys2.fish
@@ -3,5 +3,7 @@ git config --global user.name "test"
 curl -OL https://github.com/msys2/MSYS2-packages/archive/refs/heads/master.zip
 bsdtar xvf master.zip
 cd MSYS2-packages-master/msys2-runtime
-sed -i '/apply_git_am_with_msg /ised -i \'s/NAME_MAX 255/NAME_MAX 1024/\' winsup/cygwin/include/cygwin/limits.h' PKGBUILD
-# MSYSTEM=msys2 makepkg --skipchecksums -s --noconfirm
+sed -i '/autogen/ased -i "/case '`'/,+6d" cygwin/msys2_path_conv.cc' PKGBUILD
+sed -i '/autogen/ased -i "/case '@'/,+4d" cygwin/msys2_path_conv.cc' PKGBUILD
+sed -i '/autogen/ased -i \'s/NAME_MAX 255/NAME_MAX 1024/\' cygwin/include/cygwin/limits.h' PKGBUILD
+MSYSTEM=msys2 makepkg --skipchecksums -s --noconfirm
